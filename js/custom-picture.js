f46edbcf57a406374ef3ce90d1025f5a74cc013d
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-03-12 17:11:03 +0100

    Добавляет функционал смены дефолтной фотографии на кастомную при загрузке

---
+++ js/custom-picture.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var imageEditor = window.util.imageEditor;
+  var imageUploadPreview = window.util.imageUploadPreview;
+
+  var uploadInput = document.querySelector('.img-upload__input');
+  var effectsPreview = imageEditor.querySelectorAll('.effects__preview');
+
+  uploadInput.addEventListener('change', function () {
+    var file = uploadInput.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (item) {
+      return fileName.endsWith(item);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        imageUploadPreview.src = reader.result;
+
+        for (var i = 0; i < effectsPreview.length; i++) {
+          effectsPreview[i].style = 'background-image: url("' + reader.result + '");';
+        }
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
